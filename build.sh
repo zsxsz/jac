d65af01a54a7f30f617439233ca32637d0110b8c
unknown <kupraa24@gmail.com>
2025-01-10 23:20:52 +0700

    Update build configuration for ARlink deployment

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Create dist directory
+# Create dist directory if it doesn't exist
 mkdir -p dist
 
+# Create a directory for Java build artifacts
+mkdir -p dist/java
+
 # Compile Java files
-javac -d dist src/**/*.java
+javac -d dist/java src/**/*.java
 
 # Copy resources
-cp -r src/card_images dist/
+cp -r src/card_images dist/java/
 
 # Create JAR file
-cd dist
-jar cfm Blackjack.jar ../tools/Blackjack.mf *.class Cards/*.class Players/*.class card_images/*
-cd ..
+cd dist/java
+jar cfm ../Blackjack.jar ../../tools/Blackjack.mf *.class Cards/*.class Players/*.class card_images/*
+cd ../..
